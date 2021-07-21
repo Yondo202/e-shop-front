1b31f510ecf6b1b6d346ecf2b162eadda2c74f96
Yondo202 <yondooo61@gmail.com>
2021-07-21 10:38:06 +0800

    dd

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     env: {
-        serverUrl: 'http://192.168.88.78:1338',
+        // serverUrl: 'http://192.168.88.78:1338',
+        serverUrl: 'http://188.166.188.164:1338',
         // serverUrl: 'http://localhost:1338',
         // serverUrl: 'http://192.168.0.105:1338',
         frontUrl: 'http://localhost',
@@ -11,25 +12,3 @@ module.exports = {
         newsUrl: '/news/',
     }
 }
-
-// module.exports = {
-//     compress: false,
-// }
-
-// const securityHeaders = []
-
-// module.exports = {
-//   async headers() {
-//     return [
-//       {
-//         // Apply these headers to all routes in your application.
-//         source: '/(.*)',
-//         headers: securityHeaders,
-//       },
-//     ]
-//   },
-// }
-
-// module.exports = {
-//     generateEtags: false,
-// }
