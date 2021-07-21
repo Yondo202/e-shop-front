dcf706c639259ca43850827cb050b0633795d3ec
Yondo202 <yondooo61@gmail.com>
2021-07-21 10:44:25 +0800

    dd

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     env: {
-        // serverUrl: 'http://192.168.88.78:1338',
-        serverUrl: 'http://188.166.188.164:1338',
+        serverUrl: 'http://192.168.88.78:1338',
+        // serverUrl: 'http://188.166.188.164:1338',
         // serverUrl: 'http://localhost:1338',
         // serverUrl: 'http://192.168.0.105:1338',
-        frontUrl: 'http://localhost',
+        frontUrl: 'http://localhost:4000',
         // frontUrl: 'https://e-shop-front.vercel.app',
         // frontUrl: 'https://e-shop-front.herokuapp.com',
         // frontUrl: 'http://188.166.188.164:4000',
